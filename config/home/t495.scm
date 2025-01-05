434a4388e33fad39a2dbbda3c2ea507591914598
forgoty <forgoty13@gmail.com>
2025-01-05 22:50:56 +0100

    Include profile content file to home configuration

--- config/home/t495.scm
+++ config/home/t495.scm
@@ -201,59 +201,21 @@
   ;; services, run 'guix home search KEYWORD' in a terminal.
   (services
    (list
-     (service home-zsh-service-type)
+     (service home-zsh-service-type
+	      (home-zsh-configuration
+		(environment-variables
+		  '(("GUIX_CONFIG_PATH" . "$HOME/guix-config")
+		    ("GUIX_PACKAGE_PATH" . "$GUIX_CONFIG_PATH/config/packages")
+		    ("GUILE_LOAD_PATH" . "$GUILE_LOAD_PATH:$GUIX_PACKAGE_PATH")
+		    ("ZSH_DIR" . "$HOME/.guix-home/profile/share/zsh")
+		    ("BASH_COMPLETIONS_DIR" . "$HOME/.guix-home/profile/etc/bash_completion.d")))))
+     (simple-service 'custom/home-shell-profile-service-type
+		     home-shell-profile-service-type
+		     `(,(local-file "../../files/dotfiles/.config/shell/profile")))
      (service home-dbus-service-type)
      (service home-pipewire-service-type)
      (service home-dotfiles-service-type
 	      (home-dotfiles-configuration
-		(excluded '("README.md" ".stow-local-ignore" ".zprofile"))
+		(excluded '("README.md" ".stow-local-ignore"))
 		(directories '("../../files/dotfiles"))))
-     (simple-service 'some-useful-env-vars-service
-	home-environment-variables-service-type
-	`(("EDITOR" . "nvim")
-	  ("TERMINAL" . "st")
-	  ("TERMINAL_PROG" . "st")
-	  ("BROWSER" . "qutebrowser")
-	  ("XDG_CONFIG_HOME" . "$HOME/.config")
-	  ("XDG_DATA_HOME" . "$HOME/.local/share")
-	  ("XDG_CACHE_HOME" . "$HOME/.cache")
-	  ("XINITRC" . "$XDG_CONFIG_HOME/x11/xinitrc")
-	  ("GTK2_RC_FILES" . "$XDG_CONFIG_HOME/gtk-2.0/gtkrc-2.0")
-	  ("WGETRC" . "$XDG_CONFIG_HOME/wget/wgetrc")
-	  ("INPUTRC" . "$XDG_CONFIG_HOME/shell/inputrc")
-	  ("ZDOTDIR" . "$XDG_CONFIG_HOME/zsh")
-	  ("WINEPREFIX" . "$XDG_DATA_HOME/wineprefixes/default")
-	  ("PASSWORD_STORE_DIR" . "$XDG_DATA_HOME/password-store")
-	  ("ANDROID_SDK_HOME" . "$XDG_CONFIG_HOME/android")
-	  ("CARGO_HOME" . "$XDG_DATA_HOME/cargo")
-	  ("GOPATH" . "$XDG_DATA_HOME/go")
-	  ("GUIX_PACKAGE_PATH" . "$HOME/guix-config/config/packages")
-	  ("GUILE_LOAD_PATH" . "$GUILE_LOAD_PATH:$(echo $GUIX_PACKAGE_PATH)")
-	  ("GOMODCACHE" . "$XDG_CACHE_HOME/go/mod")
-	  ("ZSH_DIR" . "$HOME/.guix-home/profile/share/zsh")
-	  ("BASH_COMPLETIONS_DIR" . "$HOME/.guix-home/profile/etc/bash_completion.d")
-	  ("ANSIBLE_CONFIG" . "$XDG_CONFIG_HOME/ansible/ansible.cfg")
-	  ("UNISON" . "$XDG_DATA_HOME/unison")
-	  ("HISTFILE" . "$XDG_DATA_HOME/history")
-	  ("MBSYNCRC" . "$XDG_CONFIG_HOME/mbsync/config")
-	  ("ELECTRUMDIR" . "$XDG_DATA_HOME/electrum")
-	  ("PYTHONSTARTUP" . "$XDG_CONFIG_HOME/python/pythonrc")
-	  ("SQLITE_HISTORY" . "$XDG_DATA_HOME/sqlite_history")
-	  ("DICS" . "/usr/share/stardict/dic/")
-	  ("SUDO_ASKPASS" . "$HOME/.local/bin/dmenupass")
-	  ("FZF_DEFAULT_OPTS" . "--layout=reverse --height 40%")
-	  ("LESS" . "R")
-	  ("LESS_TERMCAP_mb" . "$(printf '%b' '[1;31m')")
-	  ("LESS_TERMCAP_md" . "$(printf '%b' '[1;36m')")
-	  ("LESS_TERMCAP_me" . "$(printf '%b' '[0m')")
-	  ("LESS_TERMCAP_so" . "$(printf '%b' '[01;44;33m')")
-	  ("LESS_TERMCAP_se" . "$(printf '%b' '[0m')")
-	  ("LESS_TERMCAP_us" . "$(printf '%b' '[1;32m')")
-	  ("LESS_TERMCAP_ue" . "$(printf '%b' '[0m')")
-	  ("QT_QPA_PLATFORMTHEME" . "gtk2") ; Have QT use gtk2 theme.
-	  ("MOZ_USE_XINPUT2" . #t) ; Mozilla smooth scrolling/touchpads
-	  ("AWT_TOOLKIT" . "MToolkit wmname LG3D") ; May have to install wmname
-	  ("PATH" . "$PATH:$(echo $GOPATH/bin)")
-	  ("PATH" . "$PATH:$(echo $HOME/.local/bin)")
-	  ("PATH" . "$PATH:$(echo $HOME/.local/bin/statusbar)")
-	  ("_JAVA_AWT_WM_NONREPARENTING" . #t))))))
+     )))
