3cca75da96e13059290a9c8858cc46a144f0edf9
forgoty <forgoty13@gmail.com>
2025-09-08 00:21:18 +0200

    Add guldan system configuration

---
+++ guix/forgoty/home/guldan.scm
@@ -0,0 +1,134 @@
+(define-module (forgoty home guldan)
+  #:use-module (gnu home)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages fonts)
+  #:use-module (gnu packages shellutils)
+  #:use-module (gnu packages file)
+  #:use-module (gnu packages rust-apps)
+  #:use-module (gnu packages disk)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages terminals)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages moreutils)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages rsync)
+  #:use-module (gnu packages pulseaudio)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages wget)
+  #:use-module (gnu packages suckless)
+  #:use-module (gnu services xorg)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services desktop)
+  #:use-module (gnu home services sound)
+  #:use-module (gnu home services ssh)
+  #:use-module (gnu home services dotfiles)
+  #:use-module (gnu home services mcron)
+  #:use-module (gnu home services shells)
+  #:use-module (gnu home services shells)
+  #:use-module (forgoty home services sunshine)
+  #:use-module (forgoty home services containers)
+  #:use-module ((forgoty systems base-system) #:select (default-keyboard-layout)))
+
+(define (home-guldan-dotfiles-configuration config)
+  (append
+    (home-dotfiles-configuration->files (home-dotfiles-configuration
+          (excluded '("README.md"
+                ".stow-local-ignore"
+                ".xinitrc"
+                ".xprofile"
+                ".emacs.d"))
+          (directories '("../../../dotfiles"))))
+    `(( ".xinitrc" ,(plain-file "xinitrc" "exec xterm")))))
+
+(define home-guldan-dotfiles-service-type
+  (service-type (name 'home-dotfiles)
+                (description "Dotfiles configuration")
+                (extensions (list (service-extension home-files-service-type
+                                                     home-guldan-dotfiles-configuration)
+                                  (service-extension home-shell-profile-service-type
+                                                     (const (list (plain-file
+                                                                   "startx"
+                                                                   "[ $(tty) = /dev/tty1 ] && exec startx"))))))
+                (default-value #f)))
+
+(define-public guldan-home
+  (home-environment
+   (packages (list
+              ;; Xorg
+              util-linux
+              xdg-utils
+              xdg-desktop-portal-gtk
+              setxkbmap
+              xset
+              xmodmap
+              xrandr
+              xrdb
+              xauth
+              xprop
+              xwininfo
+              xf86-input-joystick
+              lm-sensors
+              xdotool
+              xclip
+              xcompmgr
+              font-google-noto
+              font-google-noto-emoji
+              dwm
+              xterm
+
+              ;; Shell
+              zsh-syntax-highlighting
+              zsh-completions
+              file
+              bat
+              lf
+              git
+              wget
+              fzf
+              maim
+              ripgrep
+              zip
+              moreutils
+              unzip
+              htop
+              rsync
+              pulsemixer
+              dosfstools
+              curl
+              mediainfo
+
+              ;; Apps
+              flatpak
+              mpv))
+
+    (services (append %base-home-services
+                      (list
+                        ;; Pipewire
+                        (service home-pipewire-service-type)
+
+                        ;; Shell
+                        (service home-zsh-service-type)
+
+                        ;; Dotfiles
+                        (service home-guldan-dotfiles-service-type)
+
+                        (service home-dbus-service-type)
+
+                        (service home-startx-command-service-type
+                                  (xorg-configuration (keyboard-layout
+                                                      default-keyboard-layout)))
+
+                        (service home-x11-service-type)
+
+                        (service home-sunshine-service-type
+                                (home-sunshine-configuration
+                                  (config-file-path "~/.config/sunshine/sunshine.conf")))
+
+                        ;; Podman
+                        (service podman-service-type))))))
---
+++ guix/forgoty/systems/guldan.scm
@@ -0,0 +1,153 @@
+(define-module (forgoty systems guldan)
+  #:use-module (gnu)
+  #:use-module (gnu system)
+  #:use-module (gnu system accounts)
+  #:use-module (gnu services base)
+  #:use-module (gnu services guix)
+  #:use-module (gnu packages linux)
+  #:use-module (guix store)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (nongnu packages linux)
+  #:use-module (nongnu system linux-initrd)
+  ;; #:use-module (nongnu services nvidia)
+  #:use-module (forgoty services sunshine)
+  #:use-module (forgoty home guldan)
+  #:use-module (forgoty systems base-system))
+
+(use-service-modules desktop
+                     linux
+                     xorg
+                     audio
+                     ssh
+                     virtualization
+                     containers
+                     networking)
+
+(use-package-modules admin
+                     fonts
+                     gnome
+                     linux
+                     audio
+                     file-systems
+                     vim
+                     nfs
+                     shells
+                     video
+                     xorg
+                     package-management)
+(define evdi-linux
+  (package
+    (inherit evdi)
+    (name "evdi")
+    (version "1.14.10")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/DisplayLink/evdi")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1ysllh2qnkllvq7k1w2gpn6qqszra08s4ywsqcppzzbd1wgc07f4"))))
+    (arguments
+     (list #:tests? #f
+           #:linux linux
+           #:phases #~(modify-phases %standard-phases
+                        (add-after 'unpack 'chdir
+                          (lambda _
+                            (chdir "module"))))))))
+
+(define system-packages
+  (list bluez
+        bluez-alsa
+        evdi-linux
+        fuse-exfat
+        ntfs-3g
+        xf86-video-dummy
+        libva
+        libva-utils
+        neovim))
+
+(define system-services
+  (list (service openssh-service-type
+                 (openssh-configuration (port-number 2222)))
+        (service bluetooth-service-type
+                 (bluetooth-configuration (auto-enable? #t)))
+        (service sunshine-service-type)
+        (service iptables-service-type)
+        ;; (service nvidia-service-type)
+        (service guix-home-service-type `(("nikita" ,guldan-home)))
+        (service kernel-module-loader-service-type '("evdi"))
+        (simple-service 'evdi-config etc-service-type
+                        (list `("modprobe.d/evdi.conf"
+                                ,(plain-file "evdi.conf"
+                                             "options evdi initial_device_count=1"))))
+        (service rootless-podman-service-type
+          (rootless-podman-configuration
+            (podman #f)
+            (subgids
+              (list (subid-range (name "nikita"))))
+            (subuids
+              (list (subid-range (name "nikita"))))))))
+
+(define guldan
+  (operating-system
+    (locale "en_US.utf8")
+    (timezone "Europe/Warsaw")
+    (host-name "guldan")
+    (kernel linux)
+    (initrd microcode-initrd)
+    (firmware (list linux-firmware))
+    (kernel-arguments (cons* "evdi.initial_device_count=1"
+                             %default-kernel-arguments))
+    (users (append
+            (list (user-account
+              (name "nikita")
+              (comment "Nikita")
+              (password (crypt "password" "$6$abc"))
+              (group "users")
+              (home-directory "/home/nikita")
+              (shell (file-append zsh "/bin/zsh"))
+              (supplementary-groups '("wheel"
+                                      "netdev"
+                                      "audio"
+                                      "input"
+                                      "tty"
+                                      "video"
+                                      "lp"
+                                      "cgroup"
+                                      "kvm"))))
+            %base-user-accounts))
+    (packages (append system-packages %base-packages))
+    (services
+      (append system-services
+              (modify-services %desktop-services
+                (delete gdm-service-type)
+                (mingetty-service-type config =>
+                  (mingetty-configuration
+                    (inherit config)
+                    (auto-login "nikita")))
+                (guix-service-type config =>
+                                  (guix-configuration (inherit config)
+                                                      (substitute-urls (append
+                                                                        (list
+                                                                          "https://nonguix-proxy.ditigal.xyz")
+                                                                        %default-substitute-urls))
+                                                      (authorized-keys (append
+                                                                        (list (local-file
+                                                                                "./nonguix-signing-key.pub"))
+                                                                        %default-authorized-guix-keys)))))))
+
+    (file-systems (append (list
+                           (file-system
+                             (device "/dev/vda1")
+                             (mount-point "/")
+                             (type "btrfs")))
+                        %base-file-systems))
+    (bootloader (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (targets '("/boot/efi"))
+      (timeout 0)))))
+
+guldan
