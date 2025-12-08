32e78ae51a099bf9e79957c73bf8e1e8db75588b
forgoty <forgoty13@gmail.com>
2025-12-08 22:59:35 +0100

    do not include version for testing into repo

--- guix/forgoty/home/t495.scm
+++ guix/forgoty/home/t495.scm
@@ -1,7 +1,7 @@
 (define-module (forgoty home t495)
   #:use-module (gnu home)
-  #:use-module (forgoty home test-home)
   #:use-module (forgoty home common-desktop))
 
 (home-environment
-  (services (append common-desktop-home-services test-home-services)))
+  (services
+   (append common-desktop-home-services %base-home-services)))
