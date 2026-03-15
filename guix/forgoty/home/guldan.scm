fc54cc784282039d36f24fa206c7c501b5ea8695
forgoty <forgoty13@gmail.com>
2026-03-15 17:30:40 +0100

    add guix sandbox extra shares env var with games

--- guix/forgoty/home/guldan.scm
+++ guix/forgoty/home/guldan.scm
@@ -237,6 +237,7 @@
           (list '("GDK_SCALE" . "2")
                 '("GDK_DPI_SCALE" . "1")
                 '("QT_SCALE_FACTOR" . "2")
+                '("GUIX_SANDBOX_EXTRA_SHARES" . "/home/nikita/Games")
                 '("QT_AUTO_SCREEN_SCALE_FACTOR" . "0")
                 '("XINITRC" . ".xinitrc"))))
 
