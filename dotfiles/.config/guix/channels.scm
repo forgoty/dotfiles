7a2c977412fccb045abf405ea78e41e0ef76e871
forgoty <forgoty13@gmail.com>
2026-04-06 15:02:47 +0200

    add guix-science-nonfree channel

--- dotfiles/.config/guix/channels.scm
+++ dotfiles/.config/guix/channels.scm
@@ -15,4 +15,13 @@
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
             (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
+             "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+
+      (channel
+        (name 'guix-science-nonfree)
+        (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
+        (introduction
+        (make-channel-introduction
+          "58661b110325fd5d9b40e6f0177cc486a615817e"
+          (openpgp-fingerprint
+          "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446")))))
