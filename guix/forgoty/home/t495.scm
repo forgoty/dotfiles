3e508338b1a5cca3714512b195ade97cb426047e
forgoty <forgoty13@gmail.com>
2026-06-16 22:33:02 +0200

    add steam to t495

--- guix/forgoty/home/t495.scm
+++ guix/forgoty/home/t495.scm
@@ -2,12 +2,14 @@
   #:use-module (gnu home)
   #:use-module (gnu services)
   #:use-module (gnu home services)
+  #:use-module (nongnu packages game-client)
   #:use-module (forgoty home common-desktop)
   #:use-module (forgoty home services dotfiles))
 
 (define %host "t495")
 
 (home-environment
+  (packages (list steam))
   (services
    (append (list (service home-forgoty-dotfiles-service-type
                           (home-forgoty-dotfiles-configuration (host %host))))
