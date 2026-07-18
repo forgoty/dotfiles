0d0cc994c6cd2dab911d31a6760bfd890ad984ae
forgoty <forgoty13@gmail.com>
2026-07-18 13:45:26 +0200

    clean arch envs

--- guix/forgoty/home/arch.scm
+++ guix/forgoty/home/arch.scm
@@ -49,10 +49,8 @@
                 '("REBOOT" . "sudo /usr/bin/reboot")
                 '("DOCKER" . "docker")
                 '("SUSPEND" . "sudo /usr/bin/systemctl suspend")
-		'("CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC". "1")
-		'("CLAUDE_CODE_IDE_SKIP_AUTO_INSTALL". "1")
-		'("MCP_TIMEOUT". "600000")
-		'("USE_BUILTIN_RIPGREP". "0"))))
+                '("MCP_TIMEOUT". "600000")
+                '("USE_BUILTIN_RIPGREP". "0"))))
 
 (define home-arch-desktop-services
   (append (list
